dd5208d840574791fd456f6ea8ef25b2d24cc642
谢亚波 <1047794348@qq.com>
2019-03-25 13:19:03 +0800

    Release 2.0.0

--- XYBCategary/NSDate+Untiliy.h
+++ XYBCategary/NSDate+Untiliy.h
@@ -6,11 +6,13 @@
 //  Copyright © 2019 谢亚波. All rights reserved.
 //
 
-
+#import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSDate (Untiliy)
+
++ (NSString *)xyb_dateToStr:(NSString *) dateTime formatter:(NSString *)formatter;
 
 @end
 
